eb407ab16238b4692b598310aaab1f2ef29f140c
Mark Allen <maallen87@gmail.com>
2014-03-09 23:56:32 +0000

    First implementation with MongoDB

--- QuizzyRascalServer/src/main/java/com/grimewad/quizzyrascal/QuizzyRascalServer.java
+++ QuizzyRascalServer/src/main/java/com/grimewad/quizzyrascal/QuizzyRascalServer.java
@@ -1,10 +1,18 @@
 package com.grimewad.quizzyrascal;
 
+import java.net.UnknownHostException;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 
 @Path("/")
 public class QuizzyRascalServer {
@@ -12,12 +20,12 @@ public class QuizzyRascalServer {
 	public static int DEVICE_ID = 0;
 	
 	@POST
-	@Path("/notifyServer")
+	@Path("notifyServer")
 	@Consumes("application/json")
 	public void receiveNotificationFromClientDevice(Notification notification){
 		System.out.println("Received notification for device "
 				+ notification.getDeviceId() + ", running browser "
-						+ notification.getOpenBrowser());
+						+ notification.getOpenBrowser());	
 	}
 	
 	@GET
@@ -26,7 +34,31 @@ public class QuizzyRascalServer {
 	public DeviceId assignDeviceId(){
 		DeviceId deviceId = new DeviceId();
 		deviceId.setDeviceId(generateDeviceId());
+		DB db = getMongoDb();
+		BasicDBObject dbObject = createDbObject(deviceId.getDeviceId());
+		DBCollection devices = db.getCollection("Devices");
+		devices.insert(dbObject);
 		return deviceId;
+	}
+	
+	private BasicDBObject createDbObject(int deviceId){
+		return new BasicDBObject().append("deviceId", deviceId)
+				.append("teamName", "testTeam");
+	}
+	
+	private DB getMongoDb(){
+		String mongoDB = "mongodb://root:shroot@ds031349.mongolab.com:31349/quizzyrascal";
+		MongoClientURI mongoClientURI = new MongoClientURI(mongoDB);
+		MongoClient mongoClient = null;
+		try {
+			mongoClient = new MongoClient(mongoClientURI);
+			System.out.println("Connected!!");
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return mongoClient.getDB("quizzyrascal");
 	}
 	
 	@GET
